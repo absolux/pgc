077d0438965017eefd9fe7ed7514370aa2a2dfed
absolux <a.mouhassine@gmail.com>
2019-02-11 01:06:42 +0100

    Fix tests

--- test/unit/async-emitter.js
+++ test/unit/async-emitter.js
@@ -1,16 +1,16 @@
 
 const assert = require('assert')
-const { AsyncEmitter } = require('../../lib/utils/async-emitter')
+const { AsyncEmitter } = require('../../lib/util/async-emitter')
 
-// describe('Test the async emitter', () => {
-//   it('should emit invoke listeners in correct order', () => {
+describe('Test the async emitter', () => {
+  it('should emit invoke listeners in correct order', () => {
     let i = 1
     let ee = new AsyncEmitter()
 
-    ee.on('foo', () => console.log(i++))
-    ee.on('foo', () => console.log(i++))
-    ee.on('foo', () => console.log(i++))
+    ee.on('foo', () => i++)
+    ee.on('foo', () => i++)
+    ee.on('foo', () => assert.equal(i, 3))
 
     ee.emit('foo')
-//   })
-// })
+  })
+})
