b339d13684bfb987647528d69d28e6865bfba16e
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2020-12-25 14:44:13 +0100

    Update and rename queue.js to queue.ts

--- ___pool/queue.ts
+++ ___pool/queue.ts
@@ -1,23 +1,20 @@
 
-'use strict'
+export class Queue<T> {
+  private head?: Node<T>
+  private tail?: Node<T>
 
-export class Queue {
-  constructor () {
-    this.head = this.tail = null
-  }
-
-  isEmpty () {
+  isEmpty (): boolean {
     return this.head == null
   }
 
-  push (value) {
+  push (value: T): void {
     this.tail = createNode(value, this.tail)
 
     if (! this.head) this.head = this.tail
   }
 
-  shift () {
-    if (! this.head) return
+  shift (): T | undefined {
+    if (this.isEmpty()) return
 
     let node = this.head
 
@@ -26,7 +23,9 @@ export class Queue {
     return node.value
   }
 
-  each (fn) {
+  each (fn: (n: Node<T>) => any): void {
+    if (this.isEmpty()) return
+
     let node = this.head
 
     do fn.call(null, node)
@@ -34,8 +33,13 @@ export class Queue {
   }
 }
 
-function createNode (value, previous) {
+function createNode<T> (value: T, previous?: Node<T>): Node<T> {
   if (! previous) return { value }
 
   return previous.next = { value }
+}
+
+interface Node<T> {
+  next?: Node
+  value: T
 }
